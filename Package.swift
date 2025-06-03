847db08f65121661c476c29b86fc53882ec2359d
alirezat775 <alireza@nativeblocks.io>
2025-06-04 01:04:48 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,22 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "Nativeblocks",
-    platforms: [.macOS(.v13), .iOS(.v15)],
-    products: [
-        .library(
-            name: "Nativeblocks",
-            targets: ["NativeblocksPrivate"]
-        )
-    ],
+    platforms: [.iOS(.v15), .macOS(.v13)],
+    products: [.library(name: "Nativeblocks", targets: ["Nativeblocks"])],
     targets: [
         .binaryTarget(
             name: "Nativeblocks",
             url: "https://github.com/nativeblocks/nativeblocks-ios-sdk/releases/download/1.3.2/Nativeblocks.xcframework.zip",
             checksum: "c21c272e967e8065ca98aeec30a1e41ebe73a325d3e3af5cb6eb26ddf4021757"
-        ),
-        .target( 
-            name: "NativeblocksPrivate",
-            path: "./Sources/Empty"
         )
     ]
 )
